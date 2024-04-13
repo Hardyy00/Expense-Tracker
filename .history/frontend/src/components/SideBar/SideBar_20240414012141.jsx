14bf1d6f1022e0f7e1958c814af5686f5f04a611
Hardik Gaur <hardikgaur8901@gmail.com>
2024-04-14 01:22:32 +0530

    fixed minor bugs

--- .history/frontend/src/components/SideBar/SideBar_20240414012141.jsx
+++ .history/frontend/src/components/SideBar/SideBar_20240414012141.jsx
@@ -11,6 +11,7 @@ import { apiConnector } from "../../Operations/apiConnector";
 //   { path: "/dashboard/visualization", name: "Visualization" },
 // ];
 
+
 const links = [
   { id: "l1", path: "/", name: "DashBoard" },
   { id: "l2", path: "/visualization", name: "Visualization" },
@@ -52,6 +53,7 @@ const SideBar = () => {
   };
 
   return (
+
     <div className="bg-[#03071e] w-full h-full px-4 py-8 flex flex-col items-center justify-between">
       <div className="flex flex-col item-center justify-center gap-[8rem]">
         <div className="flex gap-4">
@@ -81,58 +83,56 @@ const SideBar = () => {
             </NavLink>
           ))}
         </div>
+
+    <div className="bg-[#03071e] w-full h-[100vh] px-4 py-8 flex flex-col items-center justify-between">
+      <div className="flex gap-4">
+        <div className="w-[3rem] h-[3rem] bg-white rounded-full">
+          <img
+            src={userDetails&&userDetails.profilePhoto}
+            alt=""
+            className="w-full h-full object-cover rounded-full"
+          />
+        </div>
+        <div>
+          <h2>Hey</h2>
+          <h1>{ userDetails&&(userDetails.gender=='Male')?'Mr.':'Mrs.'}{userDetails && userDetails.name}</h1>
+        </div>
       </div>
 
-      <div className="bg-[#03071e] w-full h-[100vh] px-4 py-8 flex flex-col items-center justify-between">
-        <div className="flex gap-4">
-          <div className="w-[3rem] h-[3rem] bg-white rounded-full">
-            <img
-              src={userDetails && userDetails.profilePhoto}
-              alt=""
-              className="w-full h-full object-cover rounded-full"
-            />
-          </div>
-          <div>
-            <h2>Hey</h2>
-            <h1>
-              {userDetails && userDetails.gender == "Male" ? "Mr." : "Mrs."}
-              {userDetails && userDetails.name}
-            </h1>
-          </div>
-        </div>
-
-        <div className="flex flex-col gap-4">
-          {" "}
-          {/* Removed extra '<' here */}
-          <NavLink
-            to="/dashboard"
-            className={({ isActive }) =>
-              isActive ? "text-[102%] flex" : "text-[#5d6383] flex"
-            }
-          >
-            <MdDashboard className="sm:mr-[1rem] sm:text-white flex text-[20px]" />
-            DashBoard
-          </NavLink>
-          <NavLink
-            to="/dashboard/visualization"
-            className={({ isActive }) =>
-              isActive ? "text-[102%] flex" : "text-[#5d6383] flex"
-            }
-          >
-            <PiGraphFill className="sm:mr-[1rem] sm:text-white text-[20px]" />
-            Visualization
-          </NavLink>
-        </div>
-
+      <div className="flex flex-col gap-4">
+        {" "}
+        {/* Removed extra '<' here */}
         <NavLink
-          to="/"
-          onClick={handleLogout}
-          className="sm:focus:bg-sky-700 sm:flex sm:focus:text-white sm:p-[1rem] sm:rounded-full sm:pl-[2rem] sm:items-center sm:text-blue-900 sm:bold sm:p-[10px] sm:border-rounded sm:bg-red-400 sm:text-center sm:justify-center"
+          to="/dashboard"
+          className={({ isActive }) =>
+            isActive ? "text-[102%] flex" : "text-[#5d6383] flex"
+          }
         >
-          <GrLogout className=" sm:text-blue-800" />
-          Logout
+          <MdDashboard className="sm:mr-[1rem] sm:text-white flex text-[20px]" />
+          DashBoard
         </NavLink>
+        <NavLink
+          to="/dashboard/visualization"
+          className={({ isActive }) =>
+            isActive ? "text-[102%] flex" : "text-[#5d6383] flex"
+          }
+        >
+          <PiGraphFill className="sm:mr-[1rem] sm:text-white text-[20px]" />
+          Visualization
+        </NavLink>
+
       </div>
+
+      <NavLink
+        to="/"
+        onClick={handleLogout}
+        className="sm:focus:bg-sky-700 sm:flex sm:focus:text-white sm:p-[1rem] sm:rounded-full sm:pl-[2rem] sm:items-center sm:text-blue-900 sm:bold sm:p-[10px] sm:border-rounded sm:bg-red-400 sm:text-center sm:justify-center"
+      >
+        <GrLogout className=" sm:text-blue-800" />
+        Logout
+      </NavLink>
+    </div>
+
     </div>
   );
 };
