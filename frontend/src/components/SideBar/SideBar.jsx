87936b570f024d1e11a931098ebe2ce053355e1d
Manish <manishk1304prajapati@gmail.com>
2024-04-14 14:04:46 +0530

    Landing Page and DashBoard

--- frontend/src/components/SideBar/SideBar.jsx
+++ frontend/src/components/SideBar/SideBar.jsx
@@ -1,8 +1,9 @@
 import { NavLink, useNavigate } from "react-router-dom";
-import image from "../../assets/img.png";
+// import image from "../../assets/img.png";
 import Cookies from "js-cookie";
 import { GrLogout } from "react-icons/gr";
 import { MdDashboard } from "react-icons/md";
+import { FaHome } from "react-icons/fa";
 import { PiGraphFill } from "react-icons/pi";
 import { useEffect, useState } from "react";
 import { apiConnector } from "../../Operations/apiConnector";
@@ -46,28 +47,44 @@ const SideBar = () => {
   };
 
   return (
-    <div className="bg-[#03071e] w-full h-[100vh] px-4 py-8 flex flex-col items-center justify-between">
+    <div className="bg-[#03071e] w-full h-[100vh] px-4 py-8 flex flex-col items-center justify-start gap-[5rem]">
       <div className="flex gap-4">
-        <div className="w-[3rem] h-[3rem] bg-white rounded-full">
+        <div className="w-[3rem] h-[3rem] bg-white rounded-full ">
           <img
-            src={image}
+            src={userDetails && userDetails.profilePhoto}
             alt=""
             className="w-full h-full object-cover rounded-full"
           />
         </div>
         <div>
           <h2>Hey</h2>
-          <h1>{ userDetails&&(userDetails.gender=='Male')?'Mr.':'Mrs.'}{userDetails && userDetails.name}</h1>
+          <h1>
+            {userDetails && userDetails.gender == "Male" ? "Mr." : "Mrs."}
+            {userDetails && userDetails.name}
+          </h1>
         </div>
       </div>
 
-      <div className="flex flex-col gap-4">
+      <div className="flex flex-col gap-4 text-[1.5rem] ">
         {" "}
         {/* Removed extra '<' here */}
         <NavLink
+          to="/"
+          className={({ isActive }) =>
+            isActive
+              ? "text-[102%] flex items-center"
+              : "text-[#5d6383] flex  items-center"
+          }
+        >
+          <FaHome className="sm:mr-[1rem] sm:text-white flex text-[20px]" />
+          Home
+        </NavLink>
+        <NavLink
           to="/dashboard"
           className={({ isActive }) =>
-            isActive ? "text-[102%] flex" : "text-[#5d6383] flex"
+            isActive
+              ? "text-[102%] flex  items-center"
+              : "text-[#5d6383] flex  items-center"
           }
         >
           <MdDashboard className="sm:mr-[1rem] sm:text-white flex text-[20px]" />
@@ -76,22 +93,36 @@ const SideBar = () => {
         <NavLink
           to="/dashboard/visualization"
           className={({ isActive }) =>
-            isActive ? "text-[102%] flex" : "text-[#5d6383] flex"
+            isActive
+              ? "text-[102%] flex  items-center"
+              : "text-[#5d6383] flex  items-center"
           }
         >
           <PiGraphFill className="sm:mr-[1rem] sm:text-white text-[20px]" />
           Visualization
         </NavLink>
+        <NavLink
+          to="/"
+          onClick={handleLogout}
+          className={({ isActive }) =>
+            isActive
+              ? "text-[102%] flex  items-center"
+              : "text-[#5d6383] flex  items-center"
+          }
+        >
+          <GrLogout className="sm:mr-[1rem] sm:text-white text-[20px]" />
+          Logout
+        </NavLink>
       </div>
 
-      <NavLink
+      {/* <NavLink
         to="/"
         onClick={handleLogout}
         className="sm:focus:bg-sky-700 sm:flex sm:focus:text-white sm:p-[1rem] sm:rounded-full sm:pl-[2rem] sm:items-center sm:text-blue-900 sm:bold sm:p-[10px] sm:border-rounded sm:bg-red-400 sm:text-center sm:justify-center"
       >
         <GrLogout className=" sm:text-blue-800" />
         Logout
-      </NavLink>
+      </NavLink> */}
     </div>
   );
 };
